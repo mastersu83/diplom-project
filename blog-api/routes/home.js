b1ded281c325c403bb3a4ae850743d4bd7022010
mastersu83 <master_su@mail.ru>
2022-02-27 23:57:54 +0300

    server

--- blog-api/routes/home.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PostController = require("../controllers/Home");
-
-router.get("/", PostController.index);
-
-module.exports.homeRoutes = router;
--- blog-api/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controllers/User");
-
-router.get("/", UserController.all);
-router.get("/:id", UserController.show);
-
-module.exports.usersRoutes = router;

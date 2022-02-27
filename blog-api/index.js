d2caa39370a61b321f0691a30752e5f1f1756261
mastersu83 <master_su@mail.ru>
2022-02-28 01:54:13 +0300

    add blog-api

--- blog-api/index.js
+++ blog-api/index.js
@@ -19,7 +19,7 @@ app.use(
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "../build"));
 
 initRoutes(app);
 
